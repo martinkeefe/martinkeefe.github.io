b7e5501fd37746a0c7348ae78a4d1ca1de08d27c
Martin Keefe <budha57@hotmail.com>
2020-10-29 00:55:52 +0000

    Use Snowpack.

---
+++ snowpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    mount: {
+        public: '/',
+        src: '/_dist_',
+    },
+    plugins: [
+    ],
+    install: [
+        /* ... */
+    ],
+    installOptions: {
+        /* ... */
+    },
+    devOptions: {
+        open: "none"
+    },
+    buildOptions: {
+        out: './docs'
+    },
+    proxy: {
+        /* ... */
+    },
+    alias: {
+        /* ... */
+    },
+};
